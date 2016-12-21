453cbebb78982acee730cc593dbada659f9727a1
KuoTeng, Ding <rapirent@gmail.com>
2016-12-21 15:54:17 +0800

    update via web

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # OS-hw4
+## summary
+-  author:丁國騰(Kuo Teng, Ding)
+-  student_id:E94036209
+-  class:資工三乙
+-  title:hw4
+
+##  Usage:
+-  makefile:
+
+```
+  gcc -o hw4 hw4.c
+```
+
+- start the program:
+    - use redirecte（資料流重導向）
+```
+    ./hw4 < [input_file] > [output_file]
+```
+
+
+## description&programming logic:
+## place replacement policies' pros and cons:
+### LRU
+#### 優點
+- 較為準確，使page fault的比率較少發生
+#### 缺點
+- 需要有一個欄位維護page之進入時間（hit時更新），故有額外的空間需求
+- 每次page fault發生時，都得尋找最少被用到的page（以迴圈尋找進入時間最久者），故有O(N)的cost（N為page數量）
+### Random
+#### 優點
+- 沒有額外的運算或時間成本，因為只是單純的以亂數選擇替換者
+#### 缺點
+- 全憑運氣做出選擇，使得page fault rate時好時壞
+### FIFO
+#### 優點
+- 雖然有額外的空間成本，但發生page fault時只需要O(1)的時間找到替換者（因為只是從queue中pop出）
+- 相較random更好
+#### 缺點
+- 不一定最先進入的就最少被用，故沒有LRU來的精確
---
+++ hw4.c
@@ -0,0 +1,210 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#define MAX_VP 10000
+#define MAX_PF 100
+struct page{
+    int PFN;
+    int in_use;
+    int present;
+    int lru;
+};
+struct page VPN[MAX_VP];
+struct node{
+    int virtual_number;
+    struct node* next;
+};
+//PFN[PFN][In-Use][Present]
+//present: 1 for physical memory
+//PFN:page-out to disk K(when present = 0) page-in the physical memory
+//in_use: is it in virtual memory
+int main(void)
+{
+    srand(time(NULL));
+    char tmp[1000] = {'\0'};
+    char policy_str[20] = {'\0'};
+    int policy = 0;
+    //1: FIFO, 2: LRU, 3: Random
+    int vp_number;
+    int pf_number;
+    int PF[MAX_PF] = {0};
+    int disk[MAX_VP] = {0};
+    fgets(tmp,1000,stdin);
+    sscanf(tmp,"Policy: %s",policy_str);
+    memset(tmp, 0, sizeof(tmp));
+    if(strcmp(policy_str,"FIFO")==0)
+    {
+        policy = 1;
+    }
+    else if(strcmp(policy_str,"LRU")==0)
+    {
+        policy = 2;
+    }
+    else if(strcmp(policy_str, "Random")==0)
+    {
+        policy = 3;
+    }
+    struct node* queue_top = NULL;
+    struct node* queue_last = NULL;
+    //queue:queue_top->3->2->queue_last
+    fgets(tmp,1000,stdin);
+    sscanf(tmp,"Number of Virtual Page: %d\n",&vp_number);
+    memset(tmp, 0, sizeof(tmp));
+    fgets(tmp,1000,stdin);
+    sscanf(tmp,"Number of Physical Frame: %d\n",&pf_number);
+    memset(tmp, 0, sizeof(tmp));
+    int clean;
+    for(clean=0;clean<vp_number;clean++)
+    {
+        VPN[clean].PFN = -1;
+        VPN[clean].in_use = 0;
+        VPN[clean].present = 0;
+        VPN[clean].lru = 0;
+    }
+    fgets(tmp,1000,stdin);
+    memset(tmp, 0, sizeof(tmp));
+    int reference;
+    float pf_count=0;
+    int reference_count = 0;
+    int most_use = 0;
+    int source, destination, evicted_vpn;
+    while(fgets(tmp,1000,stdin)!=NULL)
+    {
+        sscanf(tmp,"Reference %d",&reference);
+        memset(tmp, 0, sizeof(tmp));
+        VPN[reference].in_use = 1;
+        reference_count = reference_count + 1;
+        VPN[reference].lru = reference_count;
+        if(VPN[reference].present==1)
+        {
+            //hit
+            printf("Hit, %d=>%d\n",reference,VPN[reference].PFN);
+        }
+        else
+        {
+            pf_count = pf_count + 1;
+            //miss
+            int find_blank;//check if there is a space to space the page
+            int is_empty=0;
+            for(find_blank = 0; find_blank<pf_number;find_blank++)
+            {
+                if(PF[find_blank]==0)//there is a empty space
+                {
+                    is_empty=1;
+                    break;
+                }
+            }
+            if(is_empty==0)//no empty sapce
+            {
+                // printf("type1\n");
+                int check_disk = 0, disk_empty = 0;
+                for(;check_disk<MAX_VP;check_disk++)
+                {
+                    if(disk[check_disk] == 0)
+                    {
+                        disk_empty = 1;
+                        break;
+                    }
+                }
+                if(disk_empty == 1)
+                {
+                    //if disk has empty space
+                    destination = check_disk;
+                }
+                else
+                {
+                    //if there is no fatal error
+                    destination = VPN[reference].PFN;
+                }
+                ////////////////
+                if(policy==1)//FIFO
+                {
+                    if(queue_top != NULL)
+                    {
+                        evicted_vpn = queue_top->virtual_number;//the number be page-out
+                    }
+                    else
+                    {
+                        fprintf(stderr,"FIFO queue ERROR!!!!\n");
+                    }
+                    source = VPN[reference].PFN;
+                    VPN[reference].PFN = VPN[evicted_vpn].PFN;
+                    VPN[reference].present = 1;
+                    VPN[evicted_vpn].present = 0;
+                    VPN[evicted_vpn].lru = 0;
+                    VPN[evicted_vpn].PFN = destination;
+                    struct node* new_node = malloc(sizeof(struct node));
+                    new_node->virtual_number = reference;
+                    new_node->next = NULL;
+                    queue_last->next = new_node;
+                    queue_last = new_node;
+                    struct node* tmp_node = queue_top;
+                    queue_top = queue_top->next;//delete from top
+                    free(tmp_node);
+                }
+                else if(policy==2)
+                {
+                    int compare_number = reference_count, less_number = 0;
+                    int traversal = 0;
+                    for(;traversal<vp_number;traversal++)
+                    {
+                        if(VPN[traversal].lru<compare_number&&VPN[traversal].lru!=0)
+                        {
+                            compare_number = VPN[traversal].lru;
+                            less_number = traversal;
+                        }
+                    }
+                    evicted_vpn = less_number;
+                    source = VPN[reference].PFN;
+                    VPN[reference].PFN = VPN[evicted_vpn].PFN;
+                    VPN[reference].present = 1;//TODO
+                    VPN[evicted_vpn].PFN = destination;
+                    VPN[evicted_vpn].present = 0;
+                    VPN[evicted_vpn].lru = 0;
+                }
+                else if(policy==3)
+                {
+                    evicted_vpn=(rand()%vp_number)+1;
+                    source = VPN[reference].PFN;
+                    VPN[reference].PFN = VPN[evicted_vpn].PFN;
+                    VPN[reference].present = 1;
+                    VPN[evicted_vpn].PFN = destination;
+                    VPN[evicted_vpn].present = 0;
+                    VPN[evicted_vpn].lru = 0;
+                }
+                disk[source] = 0;
+                disk[destination] = 1;
+            }
+            else
+            {
+                if(policy==1)//FIFO
+                {
+                    struct node* new_node = malloc(sizeof(struct node));
+                    new_node->virtual_number = reference;
+                    new_node->next = NULL;//add from last
+                    if(queue_last!=NULL)
+                    {
+                        queue_last->next = new_node;
+                    }
+                    queue_last = new_node;
+                    if(queue_top==NULL)
+                    {
+                        queue_top = queue_last;
+                    }
+                }
+                source = VPN[reference].PFN;
+                destination = -1;
+                evicted_vpn = -1;
+                VPN[reference].PFN = find_blank;
+                VPN[reference].present = 1;
+                PF[find_blank] = 1;
+                disk[source] = 0;
+                // printf("type2\n");
+            }
+            printf("Miss, %d, %d>>%d, %d<<%d\n",VPN[reference].PFN, evicted_vpn, destination, reference, source);
+        }
+    }
+    printf("Page Fault Rate: %.3f\n",pf_count/reference_count);
+    return 0;
+}
